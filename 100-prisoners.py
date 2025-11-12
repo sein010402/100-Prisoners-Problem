12575eb9b83550e5c160894d870199f7ce650a50
전태휘 <xognl427@naver.com>
2025-11-12 16:39:36 +0900

    feature: basic

---
+++ 100-prisoners.py
@@ -0,0 +1,34 @@
+import random
+
+def main():
+    NUM_DRAWERS = 10
+    NUM_REPETITIONS = int(1E5)
+
+    print('{:15}: {:5} ({})'.format('approach', 'wins', 'ratio'))
+    for approach in PrisionersGame.approaches:
+        num_victories = 0
+        for _ in range(NUM_REPETITIONS):
+            game = PrisionersGame(NUM_DRAWERS)
+            num_victories += PrisionersGame.victory(game.play(approach))
+
+        print('{:15}: {:5} ({:.2%})'.format(
+            approach.__name__, num_victories, num_victories / NUM_REPETITIONS))
+
+
+class PrisionersGame:
+    def __init__(self):
+        return
+    def play_naive(self):
+        return
+    def play_naive_mem(self):
+        return
+    def play_optimum(self):
+        return
+    @classmethod
+    def victory(csl):
+        return
+    def play(self):
+        return
+
+if __name__ == '__main__':
+    main()
